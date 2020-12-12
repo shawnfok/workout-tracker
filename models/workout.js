ad7df31017526c9c05b65d24d4c93948f240f110
shawnfok <shawnfok@gmail.com>
2020-12-11 23:05:52 -0500

    adding server.js & models js folder

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter exercise type"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter exercise name"
+      },
+      duration: {
+        type: Number,
+        required: "Enter exercise duration"
+      },
+      weight: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
