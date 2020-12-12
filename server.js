339595d66c45ee9b6cbc0a3419e4a1327799902d
shawnfok <shawnfok@gmail.com>
2020-12-11 23:10:05 -0500

    updated server.js connection info

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
